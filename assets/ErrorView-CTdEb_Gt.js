5a1ac501d27a4008004106f660e67e963dcdcf57
rndware <rndware@users.noreply.github.com>
2025-09-16 09:05:44 +0000

    deploy: 150498f7254bc7a20431a8b823c8dff5ef56c028

--- assets/ErrorView-CTdEb_Gt.js
+++
@@ -1,1 +0,0 @@
-import{d as e,c as o,o as t,a,m as r}from"./index-oUwoRJ44.js";const s={class:"error-view"},l=e({__name:"ErrorView",setup(n){return(i,c)=>(t(),o("div",s,[a(r,{logo:"🚢",title:"Error!",subtitle:"Looks like the app sunk instead!",animation:"float-rotated"})]))}});export{l as default};
